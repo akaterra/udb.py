92dccd1ef65cfb2c9a7d253d2153384905cc64dd
akaterra <akaterraco@gmail.com>
2019-11-19 23:59:49 +0200

    0.0.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 README = open(os.path.join(os.path.dirname(__file__), 'readme.rst')).read()
 
 setup(name='udb_py',
-      version='0.0.1',
+      version='0.0.2',
       description='In-memory database',
       long_description=README,
       long_description_content_type='text/x-rst',
