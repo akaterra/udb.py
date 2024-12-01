6bebba9fb9b405369d14f090f5e270e50a5643c6
akaterra <akaterraco@gmail.com>
2024-12-01 23:18:56 +0200

    continues cluster index

--- tests/test_udb_when_selects_using_cluster_index.py
+++ tests/test_udb_when_selects_using_cluster_index.py
@@ -4,22 +4,12 @@ from udb_py.common import *
 from udb_py.udb import Udb, UdbBtreeIndex, UdbClusterIndex
 
 
-def test_should_select_using_index_and_inner_index():
-    class UdbTestBtreeIndex(UdbBtreeIndex):
-        is_called = False
-
-        def clone(self):
-            return UdbTestBtreeIndex(self.schema, self.name).safe(self._safe)
-
-        def search_by_key_eq(self, key):
-            self.is_called = True
-
-            return UdbBtreeIndex.search_by_key_eq(self, key)
-
-    udb_index_a = UdbTestBtreeIndex(['a'])
-    udb_index_ab = UdbTestBtreeIndex(['a', 'b'])
-    udb_index_ab_c = UdbTestBtreeIndex(['c'])
-    udb_index_b = UdbTestBtreeIndex(['b'])
+def test_should_select_using_index_and_inner_index(mocker):
+    search_by_key_eq_spy = mocker.spy(UdbBtreeIndex, 'search_by_key_eq')
+    udb_index_a = UdbBtreeIndex(['a'])
+    udb_index_ab = UdbBtreeIndex(['a', 'b'])
+    udb_index_ab_c = UdbBtreeIndex(['c'])
+    udb_index_b = UdbBtreeIndex(['b'])
     udb = Udb({
         'a': UdbClusterIndex(udb_index_a),
         'ab': UdbClusterIndex(udb_index_ab, udb_index_ab_c),
@@ -37,5 +27,56 @@ def test_should_select_using_index_and_inner_index():
     records = list(udb.select({'a': 1, 'b': 1, 'c': 3}))
 
     assert records == [c]
-    assert udb_index_ab.is_called is True
-    assert udb_index_ab_c.is_called is True
+    assert search_by_key_eq_spy.call_count == 2
+
+
+def test_should_select_using_index_and_not_inner(mocker):
+    search_by_key_eq_spy = mocker.spy(UdbBtreeIndex, 'search_by_key_eq')
+    udb_index_a = UdbBtreeIndex(['a'])
+    udb_index_ab = UdbBtreeIndex(['a', 'b'])
+    udb_index_ab_c = UdbBtreeIndex(['d'])
+    udb_index_b = UdbBtreeIndex(['b'])
+    udb = Udb({
+        'a': UdbClusterIndex(udb_index_a),
+        'ab': UdbClusterIndex(udb_index_ab, udb_index_ab_c),
+        'b': UdbClusterIndex(udb_index_b),
+    })
+
+    a = {'a': 1, 'b': 1, 'c': 1}
+    b = {'a': 2, 'b': 1, 'c': 2}
+    c = {'a': 1, 'b': 1, 'c': 3}
+
+    udb.insert(a)
+    udb.insert(b)
+    udb.insert(c)
+
+    records = list(udb.select({'a': 1, 'b': 1, 'c': 3}))
+
+    assert records == [c]
+    assert search_by_key_eq_spy.call_count == 1
+
+
+def test_should_select_not_using_index(mocker):
+    search_by_key_eq_spy = mocker.spy(UdbBtreeIndex, 'search_by_key_eq')
+    udb_index_a = UdbBtreeIndex(['A'])
+    udb_index_ab = UdbBtreeIndex(['A', 'b'])
+    udb_index_ab_c = UdbBtreeIndex(['c'])
+    udb_index_b = UdbBtreeIndex(['B'])
+    udb = Udb({
+        'a': UdbClusterIndex(udb_index_a),
+        'ab': UdbClusterIndex(udb_index_ab, udb_index_ab_c),
+        'b': UdbClusterIndex(udb_index_b),
+    })
+
+    a = {'a': 1, 'b': 1, 'c': 1}
+    b = {'a': 2, 'b': 1, 'c': 2}
+    c = {'a': 1, 'b': 1, 'c': 3}
+
+    udb.insert(a)
+    udb.insert(b)
+    udb.insert(c)
+
+    records = list(udb.select({'a': 1, 'b': 1, 'c': 3}))
+
+    assert records == [c]
+    assert search_by_key_eq_spy.call_count == 0
